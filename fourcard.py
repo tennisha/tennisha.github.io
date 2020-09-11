5d2fb2052ae5f5a5a4dde1dd962fd9a6a2ef2dfc
tennisha <tennisha@gmail.com>
2020-09-10 21:32:21 -0400

    update

--- fourcard.py
+++ fourcard.py
@@ -1,20 +1,37 @@
 #This program is a python implementation of the casino "carnival syle" poker card games  
 #This game is being implemented by Tennisha Martin of BlackGirlsHack
 
-#Class Deck(object):
- #   def __init__():
-        
+print ('Hello World')
+
+
+
+
 class Card:
     SUITS = ('Clubs','Diamonds','Hearts','Spades')
-    RANKS = ("nope','Ace','2','3', '4', '5', '6', '7','8', '9', '10', 'Jack', 'Queen', 'King')
+    RANKS = ('nope','Ace','2','3', '4', '5', '6', '7','8', '9', '10', 'Jack', 'Queen', 'King')
 
     def __init__(self, suit=0, rank=0):
         self.suit = suit
         self.rank = rank
 
-    def __str__(self):
-        return '{0} of {1}'.format(Card.RANKS[self.rank],Card.SUITS[self.suit])    
-
+    def printcards(self):
+        print( "{} of {}".format(self.rank,self.suit)    )
+card = Card("Clubs","Ace")
+card.printcards()
+class Deck:
+    def __init__(self, suit, rank):
+        self.cards = []
+        self.build()
+ 
+    def build (self):
+        for s in  ['Clubs','Diamonds','Hearts','Spades']:
+            for r in ['Ace','2','3', '4', '5', '6', '7','8', '9', '10', 'Jack', 'Queen', 'King']:
+                self.cards.append (Card(s,r))
+    
+    def showcards (self):
+        for c in self.cards:
+                print (self)
+    
 class Player (object):
 
     def __init__( self):
@@ -22,8 +39,13 @@ class Player (object):
         self._chips = 100
         self._bet = 0
 
-    def clearHand (my):
+    def clearHand (self):
         self._cards = []
 
-    def dealCard (my, card):
+    def dealCard (self, card):
         self._cards.append(card)
+
+
+
+
+print ('Hello World')
